3240f17ccddc011c603ae6b1bd9d643e719f7989
?ngel ?lvarez <angel@nan-tic.com>
2013-01-18 16:43:55 +0100

    [FIX] encoding

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 #This file is part account_invoice_taxes_required module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
