e9bea624d249fc05c630c009bf39d1a46e4b6bd2
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-04 12:47:55 +0100

    Migrate to version 3.4

--- tests/test_account_invoice_taxes_required.py
+++ tests/test_account_invoice_taxes_required.py
@@ -1,45 +1,24 @@
 #!/usr/bin/env python
-#This file is part account_invoice_taxes_required module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part account_invoice_taxes_required module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
 import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_depends
-from trytond.backend.sqlite.database import Database as SQLiteDatabase
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class AccountInvoiceTaxesRequiredTestCase(unittest.TestCase):
-    '''
-    Test Account Invoice Taxes Required module.
-    '''
+    'Test Account Invoice Taxes Required module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module(
             'account_invoice_taxes_required')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
-
-
-def doctest_dropdb(test):
-    database = SQLiteDatabase().connect()
-    cursor = database.cursor(autocommit=True)
-    try:
-        database.drop(cursor, ':memory:')
-        cursor.commit()
-    finally:
-        cursor.close()
 
 
 def suite():
@@ -47,9 +26,6 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountInvoiceTaxesRequiredTestCase))
     suite.addTests(doctest.DocFileSuite('scenario_invoice_taxes_required.rst',
-            setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
