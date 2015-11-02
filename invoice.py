7ff467deaf6c186bafeb9a66e799c4c4d15a2848
??ngel ??lvarez <angel@nan-tic.com>
2015-11-02 14:44:18 +0100

    fix flake8 and scenario

--- invoice.py
+++ invoice.py
@@ -1,7 +1,7 @@
 #This file is part account_invoice_taxes_required module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
-from trytond.pyson import Eval
+
 from trytond.pool import Pool, PoolMeta
 
 __all__ = ['Invoice', 'InvoiceLine']
