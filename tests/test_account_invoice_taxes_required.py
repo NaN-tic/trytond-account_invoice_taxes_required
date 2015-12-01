91381d1cac2aab91d7d51013d202cf6890132e26
resteve <resteve@zikzakmedia.com>
2015-12-01 11:39:58 +0100

    Tests Scenario

--- tests/test_account_invoice_taxes_required.py
+++ tests/test_account_invoice_taxes_required.py
@@ -2,8 +2,10 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
+import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class AccountInvoiceTaxesRequiredTestCase(ModuleTestCase):
@@ -15,4 +17,7 @@ def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountInvoiceTaxesRequiredTestCase))
+    suite.addTests(doctest.DocFileSuite('scenario_invoice_taxes_required.rst',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
