9e5a8634fd470e27fb952562137784ffef3c8024
C?dric Krier <ced@b2ck.com>
2018-01-10 15:10:02 +0100

    Fix style

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
-#This file is part account_invoice_taxes_required module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part account_invoice_taxes_required module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.pool import Pool
-from .invoice import *
+from . import invoice
 
 
 def register():
     Pool.register(
-        Invoice,
-        InvoiceLine,
+        invoice.Invoice,
+        invoice.InvoiceLine,
         module='account_invoice_taxes_required', type_='model')
